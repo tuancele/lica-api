1da2f6a487a636006411497d9bf0cf03a0db13a2
tuancele <ngovantuan.tht@gmail.com>
2026-01-28 21:12:18 +0700

    Stop tracking local/tool-only files

--- tmp_add_api_token.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-$app = require __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-Illuminate\Support\Facades\DB::statement('ALTER TABLE `users` ADD `api_token` VARCHAR(80) NULL AFTER `remember_token`');
-echo "done\n";
